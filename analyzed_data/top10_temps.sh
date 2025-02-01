2b0a4ee3144265baa26aea3efb03a137fa4b3d77
albertniyonsenga <a.niyonseng@alustudent.com>
2025-02-01 07:56:53 -0800

    Trying to craft aesthetical table

--- analyzed_data/top10_temps.sh
+++ analyzed_data/top10_temps.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-tail -n +2 ../raw_data/satelite_temperature_data.csv | sort -t, -k 3 -nr | uniq | head -10 | cut -d, -f 1,2,3 > highest_temp.csv
+
+# Data analysis and automation
+tail -n +2 ../raw_data/satelite_temperature_data.csv | sort -t, -k 3 -nr | uniq | head -11 | cut -d, -f 1,2,3 > highest_temp.csv
 
 echo "Top 10 highest temperatures have been saved to highest_temp.csv"
